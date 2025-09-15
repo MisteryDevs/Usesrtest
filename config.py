36e4d90b7026d05d0d79912608dbfc6a3f3462f4
MisteryDevs <officialbotdev@gmail.com>
2025-09-15 14:45:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ if os.path.exists("local.env"):
 API_ID = int(getenv("API_ID", "14050586")) #optional
 API_HASH = getenv("API_HASH", "42a60d9c657b106370c79bb0a8ac560c") #optional
 
+MONGO_DB_NAME = "TelegramBot"
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5738579437").split()))
 OWNER_ID = int(getenv("OWNER_ID", "5738579437"))
 MONGO_URL = getenv("MONGO_URL", "")
